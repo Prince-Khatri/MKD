17209ac424bd73ec60432ed69418ad592a7f0534
Prince Khatri <151055926+Prince-Khatri@users.noreply.github.com>
2024-09-07 15:45:26 +0530

    About us and contact us template is added for MKD home

--- mkd/urls.py
+++ mkd/urls.py
@@ -24,6 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="MKDindex"),
+    path('contact/',views.contact,name="Contact Us"),
+    path('about/',views.about,name="About Us"),
     path('shop/',include('shop.urls')),
     path('blog/',include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
